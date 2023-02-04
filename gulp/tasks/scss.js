750a8980dbc9f213138504e5aba809e38180b4ae
Misha <mishadesignsev@yandex.ru>
2023-02-04 16:46:56 +0300

    changes | process scss

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -3,6 +3,11 @@ import dartSass from 'sass';
 import gulpSass from 'gulp-sass'
 import rename from 'gulp-rename';
 
+import cleanCss from 'gulp-clean-css';
+import webpcss from 'gulp-webpcss';
+import autoprefixer from 'gulp-autoprefixer';
+import groupCssMediaQueries from 'gulp-group-css-media-queries';
+
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
@@ -27,6 +32,27 @@ export const scss = () => {
         .pipe(sass({
             outputStyle: 'expanded'
         }))
+        .pipe(
+            groupCssMediaQueries()
+        )
+        .pipe(
+            webpcss(
+                {
+                    webpClass: ".webp",
+                    noWebpClass: ".no-webp"
+                }
+            )
+        )
+        .pipe(
+            autoprefixer({
+                grid: true,
+                overrideBrowserslist: ["last 3 versions"],
+                cascade: true
+            })
+        )
+        // Source comped css
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(cleanCss())
         .pipe(rename({
             extname: ".min.css"
         }))
